eea01673e7efe09617fdfd31e42e57ee3a8fcc2a
Peter Souter <p.souter@kainos.com>
2014-08-21 23:29:40 +0100

    Refactor to use vhost 'sensu'
    Not '/sensu', this doesn't work (I was pulling my hair for ages! :cry:)

--- puppet/manifests/classes/confmon.pp
+++ puppet/manifests/classes/confmon.pp
@@ -18,48 +18,34 @@
 #
 class confmon {
 
-## RabbitMQ
+  ## RabbitMQ
 
-  # Install rabbitmq
-  class { '::rabbitmq':
-    delete_guest_user => true,
-  }
-
-  # Create rabbitmq vhost for /sensu
-  rabbitmq_vhost { '/sensu':
-    ensure  => present,
-    require => Class['::rabbitmq'],
-  }
-
-  # Create rabbitmq user named sensu
-  rabbitmq_user { 'sensu':
-    password => 'secret',
-    require  => Class['::rabbitmq'],
-  }
-
-  # Set permissions for user sensu on vhost sensu
-  rabbitmq_user_permissions { 'sensu@/sensu':
-    configure_permission => '.*',
-    read_permission      => '.*',
-    write_permission     => '.*',
-    require              => Class['::rabbitmq'],
-  }
+class { 'rabbitmq':
+  ssl               => false,
+  delete_guest_user => true,
+}
+-> rabbitmq_vhost { 'sensu': }
+-> rabbitmq_user  { 'sensu': password => 'secret' }
+-> rabbitmq_user_permissions { 'sensu@sensu':
+  configure_permission => '.*',
+  read_permission      => '.*',
+  write_permission     => '.*',
+}
 
 ## Redis
 
-  # Install redis
+  # Install and configure redis
   class { 'redis': }
 
 ## Sensu
 
-  # Install and configure redis
-  class {'sensu':
-    server            => true,
-    api               => true,
-    rabbitmq_user     => 'sensu',
-    rabbitmq_password => 'secret',
-    client_name       => $fqdn,
-    client_address    => '192.168.50.4',
+  class { 'sensu':
+    server                   => true,
+    api                      => true,
+    purge_config             => true,
+    rabbitmq_password        => 'secret',
+    rabbitmq_host            => 'localhost',
+    rabbitmq_port            => 5672,
   }
 
   class { 'uchiwa':
